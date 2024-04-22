ab1676a24feb076f4ec2a5d8b8463d27297ccdff
dutexion <dutexion@gmail.com>
2024-04-22 19:55:45 +0900

    메인페이지

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -14,16 +14,61 @@ export const Main = () => {
             <div>
               수화 학습과 수화 통역은 <span>symbol</span>에서
             </div>
-            <BannerButton>수화 번역기</BannerButton>
+            <ButtonWrapper>
+              <BannerButton>한글 수화 번역기</BannerButton>
+              <BannerButton>영어 수화 번역기</BannerButton>
+            </ButtonWrapper>
           </BannerTextWrapper>
           <img
-            src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Hand%20gestures/Call%20Me%20Hand%20Medium-Light%20Skin%20Tone.png"
-            alt="Call Me Hand Medium-Light Skin Tone"
+            src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Hand%20gestures/Raising%20Hands.png"
+            alt="Raising Hands"
             width="400"
             height="400"
           />
         </BannerInnerWrapper>
       </Banner>
+      <ContainerWrapper>
+        <Container>
+          <div>
+            <div>수화를 배워보세요</div>
+            <div>다양한 수화 교육 콘텐츠를 통해 새로운 언어를 배워보세요.</div>
+          </div>
+          <Image
+            src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Objects/Flashlight.png"
+            alt="Flashlight"
+          />
+        </Container>
+        <Container>
+          <div>
+            <div>건의할 사항이 있나요?</div>
+            <div>여러분의 소중한 의견을 들려주세요. 함께 개선해 나갑시다.</div>
+          </div>
+          <Image
+            src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Objects/Loudspeaker.png"
+            alt="Loudspeaker"
+          />
+        </Container>
+        <Container>
+          <div>
+            <div>사용법을 알려드릴게요</div>
+            <div>이 앱의 사용 방법을 단계별로 안내해 드립니다.</div>
+          </div>
+          <Image
+            src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Objects/Page%20with%20Curl.png"
+            alt="Page with Curl"
+          />
+        </Container>
+        <Container>
+          <div>
+            <div>개발자를 소개할게요</div>
+            <div>이 앱을 만든 개발자들의 이야기를 들어보세요.</div>
+          </div>
+          <Image
+            src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Objects/Desktop%20Computer.png"
+            alt="Desktop Computer"
+          />
+        </Container>
+      </ContainerWrapper>
     </div>
   );
 };
@@ -85,4 +130,53 @@ const BannerButton = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  cursor: pointer;
+`;
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  gap: 20px;
+  height: 48px;
+`;
+
+const ContainerWrapper = styled.div`
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 100px 0;
+  gap: 100px;
+`;
+
+const Container = styled.div`
+  display: flex;
+  width: calc(50% - 50px);
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 60px;
+  &:nth-child(odd) {
+    flex-direction: row;
+  }
+  &:nth-child(even) {
+    flex-direction: row-reverse;
+  }
+  > div {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    > div:nth-child(1) {
+      font-size: 36px;
+      font-weight: 700;
+    }
+    > div:nth-child(2) {
+      font-size: 20px;
+      font-weight: 200;
+      word-break: keep-all;
+    }
+  }
+`;
+
+const Image = styled.img`
+  width: 200px;
+  height: 200px;
 `;
